8d1872ae5ec759412a8acb692fcedca9f4e4aa2d
cmseibel <cam.m.seib@gmail.com>
2022-06-26 14:50:15 -0700

    Write methods for reimbursement service

--- employee-servlet-app-main/src/main/java/com/revature/service/ReimbursementService.java
+++ employee-servlet-app-main/src/main/java/com/revature/service/ReimbursementService.java
@@ -1,11 +1,45 @@
 package com.revature.service;
 
+import java.util.List;
+
 public class ReimbursementService {
+	// TODO: ensure classes/methods match up with what we're actually working with
 	
-	// TODO: ensure this is actually what we're working with
 	private ReimbursementDao rdao;
 	
 	public ReimbursementService(ReimbursementDao rdao) {
 		this.rdao = rdao;
+	}
+	
+	public int add(Reimbursement r) {
+		return rdao.insert(r);
+	}
+	
+	public boolean approve(Reimbursement r) {
+		String tempStatus = r.getStatus();
+		r.setStatus("Approved");
+		if(rdao.update(r)) {
+			return true;
+		} else {
+			// Reset reimbursement's status
+			r.setStatus(tempStatus);
+			return false;
+		}
+	}
+	
+	public boolean deny(Reimbursement r) {
+		String tempStatus = r.getStatus();
+		r.setStatus("Denied");
+		if(rdao.update(r)) {
+			return true;
+		} else {
+			// Reset reimbursement's status
+			r.setStatus(tempStatus);
+			return false;
+		}
+	}
+	
+	public List<Reimbursement> getAll() {
+		return rdao.findAll();
 	}
 }
