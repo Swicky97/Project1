b283cb5662d29062974e5d235e1829f477fb2519
cmseibel <cam.m.seib@gmail.com>
2022-06-28 10:18:14 -0700

    Implement approve/deny in Reimbursement Service

--- employee-servlet-app-main/src/main/java/com/revature/service/ReimbursementService.java
+++ employee-servlet-app-main/src/main/java/com/revature/service/ReimbursementService.java
@@ -3,16 +3,13 @@ package com.revature.service;
 import java.util.List;
 
 import com.revature.dao.IReimbursementDao;
+import com.revature.dao.ReimbursementDao;
 import com.revature.models.Reimbursement;
 
 public class ReimbursementService {
 	// TODO: ensure classes/methods match up with what we're actually working with
 	
 	private IReimbursementDao rdao;
-	
-	public ReimbursementService() {
-		// TODO: DELETE once rdao is in project
-	}
 	
 	public ReimbursementService(ReimbursementDao rdao) {
 		this.rdao = rdao;
@@ -22,24 +19,33 @@ public class ReimbursementService {
 		return rdao.insert(r);
 	}
 	
-	public boolean approve(Reimbursement r) {
-		
-		if(rdao.update(r)) {
-			return true;
-		} else {
-			// Reset reimbursement's status
-			return false;
+	/**
+	 * 
+	 * @param r
+	 * @return
+	 */
+	public Reimbursement approve(Reimbursement r, int resolver) {
+		if(r.isReimbApproved()) return r; // already approved
+		r.setReimbApproved(true);
+		r.setReimbResolver(resolver);
+//		r.setReimbResolved(System.currentTimeMillis()); // Uncomment for actual use
+		if(!rdao.update(r)) {
+			r.setReimbApproved(false); // Reset reimbursement's status
+			r.setReimbResolved(-1L);
+			r.setReimbResolver(-1);
 		}
+		return r;
 	}
 	
-	public boolean deny(Reimbursement r) {
-		
-		if(rdao.update(r)) {
-			return true;
-		} else {
-			// Reset reimbursement's status
-			return false;
+	public Reimbursement deny(Reimbursement r, int resolver) {
+		if(r.isReimbApproved()) return r; // can't renege
+//		r.setReimbResolved(System.currentTimeMillis()); // Uncomment for actual use
+		r.setReimbResolver(resolver);
+		if(!rdao.update(r)) {
+			r.setReimbResolved(-1); // Reset reimbursement's status
+			r.setReimbResolver(-1);
 		}
+		return r;
 	}
 	
 	public List<Reimbursement> getAll() {
