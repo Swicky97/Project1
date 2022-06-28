2c7f193ca86cf2f706e0f69a128a3462d24e6231
cmseibel <cam.m.seib@gmail.com>
2022-06-28 09:46:21 -0700

    Add reimbursement dao template to mock in service tests

---
+++ employee-servlet-app-main/src/main/java/com/revature/dao/ReimbursementDao.java
@@ -0,0 +1,33 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.models.Reimbursement;
+
+public class ReimbursementDao implements IReimbursementDao {
+
+	@Override
+	public int insert(Reimbursement r) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Reimbursement> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Reimbursement r) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
