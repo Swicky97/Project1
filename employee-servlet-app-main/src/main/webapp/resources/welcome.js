45a54e05dc05b3538086b6ebe91d2c525f445223
cmseibel <cam.m.seib@gmail.com>
2022-06-28 14:08:48 -0700

    Write a add Reimbursment fetch and a form implementation on welcome.html

--- employee-servlet-app-main/src/main/webapp/resources/welcome.js
+++ employee-servlet-app-main/src/main/webapp/resources/welcome.js
@@ -1,7 +1,15 @@
 const testBtn = document.getElementById("me-btn");
+const reimbBtn = document.getElementById("reimb-btn");
 
 testBtn.addEventListener("click", async () => {
     let data = await getMe();
-
     console.log(data);
-})
+});
+
+reimbBtn.addEventListener("click", async function(e) {
+    e.preventDefault();
+    const amount = document.getElementById("amount").value;
+    const description = document.getElementById("description").value;
+    let result = await addReimbursment({amount, description});
+    console.log(result);
+});
