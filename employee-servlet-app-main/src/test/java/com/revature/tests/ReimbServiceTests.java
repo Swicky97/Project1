db7803f6ad3031493cd10d971e9289b40396d8b4
cmseibel <cam.m.seib@gmail.com>
2022-07-03 13:40:12 -0700

    Modify test cases to be compliant with newer code version

--- employee-servlet-app-main/src/test/java/com/revature/tests/ReimbServiceTests.java
+++ employee-servlet-app-main/src/test/java/com/revature/tests/ReimbServiceTests.java
@@ -1,8 +1,12 @@
 package com.revature.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.sql.Timestamp;
 
 import org.junit.After;
 import org.junit.Before;
@@ -36,16 +40,16 @@ public class ReimbServiceTests {
 	
 	@Test
 	public void testApproval_success() {
-		Reimbursement r = new Reimbursement(1, 1000, 1L, -1L, false, "haha", 1, -1);
+		Reimbursement r = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), null, false, "haha", 1, -1);
 		when(mockdao.update(r)).thenReturn(true);
 		Reimbursement actual = rserv.approve(r, 2);
-		Reimbursement expected = new Reimbursement(1, 1000, 1L, -1L, true, "haha", 1, 2);
-		assertEquals(expected, actual); // Expected to "fail" when actually using timestamps
+		Reimbursement expected = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), null, true, "haha", 1, 2);
+		assertEquals(expected.isReimbApproved(), actual.isReimbApproved()); // Expected to "fail" when actually using timestamps
 	}
 	
 	@Test
 	public void testApproval_success_alreadyApproved() {
-		Reimbursement r = new Reimbursement(1, 1000, 1L, 1L, true, "haha", 1, 2);
+		Reimbursement r = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), true, "haha", 1, 2);
 		Reimbursement actual = rserv.approve(r, 2);
 		Reimbursement expected = r;
 		assertEquals(expected, actual);
@@ -53,25 +57,24 @@ public class ReimbServiceTests {
 	
 	@Test
 	public void testApproval_fail_updateFailed() {
-		Reimbursement r = new Reimbursement(1, 1000, 1L, -1L, false, "haha", 1, -1);
+		Reimbursement r = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), null, false, "haha", 1, -1);
 		when(mockdao.update(r)).thenReturn(false);
 		Reimbursement actual = rserv.approve(r, 2);
-		Reimbursement expected = r;
-		assertEquals(expected, actual);
+		assertNull(actual.getReimbResolved());
 	}
 	
 	@Test
 	public void testDenial_success() {
-		Reimbursement r = new Reimbursement(1, 1000, 1L, -1L, false, "haha", 1, -1);
+		Reimbursement r = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), null, false, "haha", 1, -1);
 		when(mockdao.update(r)).thenReturn(true);
 		Reimbursement actual = rserv.deny(r, 2);
-		Reimbursement expected = new Reimbursement(1, 1000, 1L, -1L, false, "haha", 1, 2);
-		assertEquals(expected, actual); // Expected to "fail" when actually using timestamps
+		Reimbursement expected = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), null, false, "haha", 1, 2);
+		assertNotNull(actual.getReimbResolved()); // Expected to "fail" when actually using timestamps
 	}
 	
 	@Test
 	public void testDenial_fail_alreadyApproved() {
-		Reimbursement r = new Reimbursement(1, 1000, 1L, 1L, true, "haha", 1, 2);
+		Reimbursement r = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), true, "haha", 1, 2);
 		Reimbursement actual = rserv.deny(r, 2);
 		Reimbursement expected = r;
 		assertEquals(expected, actual);
@@ -79,7 +82,7 @@ public class ReimbServiceTests {
 	
 	@Test
 	public void testDenial_fail_updateFailed() {
-		Reimbursement r = new Reimbursement(1, 1000, 1L, -1L, false, "haha", 1, -1);
+		Reimbursement r = new Reimbursement(1, 1000, new Timestamp(System.currentTimeMillis()), null, false, "haha", 1, -1);
 		when(mockdao.update(r)).thenReturn(false);
 		Reimbursement actual = rserv.deny(r, 2);
 		Reimbursement expected = r;
